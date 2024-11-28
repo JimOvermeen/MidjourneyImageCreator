b10ec4b719205f7e43b5c7915428905f9a9aec9a
Jim <j.overmeen@dutchview.com>
2024-11-28 15:50:23 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MidjourneyService } from './services/midjourney.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'MidjourneyImageCreator';
+  prompt: string = '';
+  taskId: string = '';
+  generatedImageUrl1: string = '';
+  generatedImageUrl2: string = '';
+  generatedImageUrl3: string = '';
+  percentage: number = 0;
+
+  constructor(private midjourneyService: MidjourneyService) {}
+
+  onSubmit() {
+    this.midjourneyService.generateImage(this.prompt).subscribe({
+      next: (response) => {
+        this.taskId = response.taskId;
+        this.fetchGeneratedImage();
+      },
+      error: (error) => {
+        console.error(error.error.message);
+      },
+    });
+  }
+
+  fetchGeneratedImage() {
+    this.midjourneyService.getImage(this.taskId).subscribe({
+      next: (imageResponse) => {
+        if (
+          imageResponse.percentage === 100 &&
+          imageResponse.status === 'Finished'
+        ) {
+          this.generatedImageUrl1 = imageResponse.u1Url;
+          this.generatedImageUrl2 = imageResponse.u2Url;
+          this.generatedImageUrl3 = imageResponse.u3Url;
+        } else {
+          this.percentage = imageResponse.percentage;
+          setTimeout(() => this.fetchGeneratedImage(), 5000);
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching generated image:', error);
+        // You can add user-friendly error handling here
+      },
+    });
+  }
 }
---
+++ src/app/services/midjourney.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../env/env';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MidjourneyService {
+  private apiUrl =
+    'https://midjourney-imaginecraft-generative-ai-api.p.rapidapi.com/midjourney/imagine';
+
+  constructor(private http: HttpClient) {}
+
+  generateImage(prompt: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': environment.XRapidapiKey,
+      'x-rapidapi-host':
+        'midjourney-imaginecraft-generative-ai-api.p.rapidapi.com',
+      'Content-Type': 'application/json',
+    });
+
+    const body = {
+      Prompt: prompt,
+    };
+
+    return this.http.post(this.apiUrl, body, { headers });
+  }
+
+  getImage(taskId: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': '5ff069bd1fmshcf29bca29f2dce6p1b9930jsn53908529ca1c',
+      'x-rapidapi-host':
+        'midjourney-imaginecraft-generative-ai-api.p.rapidapi.com',
+      'Content-Type': 'application/json',
+    });
+    const url =
+      'https://midjourney-imaginecraft-generative-ai-api.p.rapidapi.com/midjourney/getresult';
+
+    return this.http.post(url, { taskId }, { headers });
+  }
+}
